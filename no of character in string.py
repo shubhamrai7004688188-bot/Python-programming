315487c6ae70ccaab2e137bc40d7fb7291d16ce0
Shubham Rai <shubhamrai7004688188@gmail.com>
2026-02-27 20:48:48 +0530

    basic idea of how python works on string

---
+++ no of character in string.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 12 22:15:28 2026
+
+@author: shubh
+"""
+
+a=input("enter the desired string ")
+count=0
+for ch in a:
+    count+=1
+print(count)    
+    
