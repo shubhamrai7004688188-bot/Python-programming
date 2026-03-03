2a263c2546ad66b15a387874e39c2cc6bde9ee0f
Shubham Rai <shubhamrai7004688188@gmail.com>
2026-03-04 00:01:07 +0530

    Add files via upload

---
+++ unique words from multi word string.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 28 12:15:44 2026
+
+@author: shubh
+"""
+
+string=("to be or not to be that is the question")
+list=string.split()
+set=set(list)
+print('unique words are',set)
